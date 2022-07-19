722475fbdd596368386bbb03c9cf190f2e1f7f84
Kashan-Akram <kashan123akram456gmail.com>
2022-07-19 20:23:32 +0530

    made some changes to main.dart

--- lib/views/Login_view.dart
+++ lib/views/Login_view.dart
@@ -11,73 +11,8 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  TextEditingController? _email;
-  TextEditingController? _password;
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email!.dispose();
-    _password!.dispose();
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register"),
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState){
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: "Enter your email",
-                    ),
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                      hintText: "Enter your password",
-                    ),
-                  ),
-
-                  TextButton(onPressed: () async {
-                    final email = _email!.text;
-                    final password = _password!.text;
-                    final usercredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                    );
-                  },
-                    child: const Text("Register"),
-                  ),
-                ],
-              );
-            default:
-              return Text("loading...");
-          }
-
-        },
-      ),
-    );
+    return container();
   }
 }

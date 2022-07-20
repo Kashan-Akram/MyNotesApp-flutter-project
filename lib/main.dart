66acdd0ef17d8fc08c37ab320774fe9755bb5a45
Kashan-Akram <kashan123akram456gmail.com>
2022-07-20 12:14:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hehewhoknows/views/Login_view.dart';
+
+import 'firebase_options.dart';
 
 
 void main() {
@@ -10,10 +13,35 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.pink,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
   ),
   );
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return const Text("Done!");
+            default:
+              return const Text("loading...");
+          }
+        },
+      ),
+    );
+  }
+
+}

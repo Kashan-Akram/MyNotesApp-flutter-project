8d9a764ae32f26a16ea5ec42a9d2ad5de8aef78c
Kashan-Akram <kashan123akram456gmail.com>
2022-07-23 16:25:40 +0530

    Update auth_providers.dart

--- lib/services/auth/auth_providers.dart
+++ lib/services/auth/auth_providers.dart
@@ -2,4 +2,14 @@ import 'package:hehewhoknows/services/auth/auth_user.dart';
 
 abstract class AuthProvider{
   AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
 }

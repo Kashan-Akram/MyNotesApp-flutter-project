e83a0d89deda4fad0661adaa37482c39cbe5bc0c
Kashan-Akram <kashan123akram456gmail.com>
2022-07-24 14:55:11 +0530

    Create auth_service.dart

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,34 @@
+import 'package:hehewhoknows/services/auth/auth_providers.dart';
+import 'package:hehewhoknows/services/auth/auth_user.dart';
+
+class AuthService implements AuthProvider{
+  final AuthProvider provider;
+  const AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) => provider.createUser(
+      email: email,
+      password: password,
+  );
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) => provider.logIn(
+      email: email,
+      password: password,
+  );
+
+  @override
+  Future<void> logOut() => provider.logOut();
+
+  @override
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
+}

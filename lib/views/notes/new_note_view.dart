41a95715d5b23d74730e93bba0ee9d870b8df5ad
Kashan-Akram <kashan123akram456gmail.com>
2022-10-28 00:59:17 +0530

    preliminary for designing main ui of new_note_view

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hehewhoknows/services/auth/auth_service.dart';
+import 'package:hehewhoknows/services/crud/notes_services.dart';
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({Key? key}) : super(key: key);
@@ -8,6 +10,66 @@ class NewNoteView extends StatefulWidget {
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
+
+  DatabaseNote? _note;
+  late final NotesServices _notesServices;
+  late final TextEditingController _textController;
+
+  @override
+  void initState() {
+    _notesServices = NotesServices();
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  void _textControllerListener() async {
+    final note = _note;
+    if(note == null){
+      return;
+    }
+    final text = _textController.text;
+    await _notesServices.updateNote(note: note, text: text);
+  }
+
+  void _setupTextControllerListener(){
+    _textController.removeListener(_textControllerListener);
+    _textController.addListener(_textControllerListener);
+  }
+
+  Future<DatabaseNote> createNewNote() async {
+    final existingNote = _note;
+    if(existingNote != null){
+      return existingNote;
+    }
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesServices.getUser(email: email);
+    return _notesServices.createNote(owner: owner);
+  }
+
+  void _deleteNoteIfTextisEmpty(){
+    final note = _note;
+    if(_textController.text.isEmpty && note != null){
+      _notesServices.deleteNote(id: note.id);
+    }
+  }
+
+  void _saveNoteIfTextNotEmpty() async {
+    final note = _note;
+    final text = _textController.text;
+    if(note != null && text.isNotEmpty){
+      await _notesServices.updateNote(note: note, text: text);
+    }
+  }
+
+  @override
+  void dispose() {
+    _deleteNoteIfTextisEmpty();
+    _saveNoteIfTextNotEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

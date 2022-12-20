d64170cdbe91200cdb0a5efeab16e83ce75ce259
Kashan-Akram <kashan123akram456gmail.com>
2022-12-21 03:30:47 +0530

    Update auth_state.dart

--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:hehewhoknows/services/auth/auth_user.dart';
 
@@ -24,11 +25,14 @@ class AuthStateNeedsVerification extends AuthState{
   const AuthStateNeedsVerification();
 }
 
-class AuthStateLoggedOut extends AuthState{
+class AuthStateLoggedOut extends AuthState with EquatableMixin {
   final Exception? exception;
   final bool isLoading;
   const AuthStateLoggedOut({
     required this.exception,
     required this.isLoading,
   });
+
+  @override
+  List<Object?> get props => [exception, isLoading];
 }

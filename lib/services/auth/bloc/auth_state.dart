5988b9bbfe6d85380c82cea4d9cb9243575ae3aa
Kashan-Akram <kashan123akram456gmail.com>
2022-12-21 03:27:29 +0530

    added some more events and states corresponding to thos events

--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -6,8 +6,13 @@ abstract class AuthState{
   const AuthState();
 }
 
-class AuthStateLoading extends AuthState{
-  const AuthStateLoading();
+class AuthStateUninitialized extends AuthState{
+  const AuthStateUninitialized();
+}
+
+class AuthStateRegistering extends AuthState{
+  final Exception? exception;
+  const AuthStateRegistering(this.exception);
 }
 
 class AuthStateLoggedIn extends AuthState{
@@ -21,10 +26,9 @@ class AuthStateNeedsVerification extends AuthState{
 
 class AuthStateLoggedOut extends AuthState{
   final Exception? exception;
-  const AuthStateLoggedOut(this.exception);
-}
-
-class AuthStateLogOutFailure extends AuthState{
-  final Exception exception;
-  const AuthStateLogOutFailure(this.exception);
+  final bool isLoading;
+  const AuthStateLoggedOut({
+    required this.exception,
+    required this.isLoading,
+  });
 }
--- lib/views/Login_view.dart
+++ lib/views/Login_view.dart
@@ -95,7 +95,7 @@ class _LoginViewState extends State<LoginView> {
             onPressed: () {
               Navigator.of(context).pushNamedAndRemoveUntil(
                 registerRoute,
-                    (route) => false,
+                (route) => false,
               );
             },
             child: const Text("Not registered yet? Register here!"),

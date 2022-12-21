73a70444fb0d5b479b3cc20d0fc70959c945bfa2
Kashan-Akram <kashan123akram456gmail.com>
2022-12-22 02:59:54 +0530

    did some changes for the verify email view to look better

--- lib/views/Verify_Email_view.dart
+++ lib/views/Verify_Email_view.dart
@@ -17,35 +17,48 @@ class _VerifyEmailState extends State<VerifyEmail> {
       appBar: AppBar(
         title: const Text("Verify Email"),
       ),
-      body: Column(
-        children: [
-          const Text("We've sent you an email verification"),
-          const Text("If verification email not received, press the button below"),
-          TextButton(
-            onPressed: () {
-              context.read<AuthBloc>().add(
-                const AuthEventSendEmailVerification(),
-              );
-              //await AuthService.firebase().sendEmailVerification();
-          },
-            child: const Text("Send Email Verification"),
-          ),
-          TextButton(
-            onPressed: () async {
-              context.read<AuthBloc>().add(
-                const AuthEventLogOut(),
-              );
-              /*
-              await AuthService.firebase().logOut();
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                  registerRoute,
-                  (route) => false,
-              );
-              */
-          },
-              child: const Text("Restart"),
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("We've sent you an email verification!"),
+            const Text("If verification email not received, press the button below"),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        context.read<AuthBloc>().add(
+                          const AuthEventSendEmailVerification(),
+                        );
+                        //await AuthService.firebase().sendEmailVerification();
+                    },
+                      child: const Text("Send Email Verification"),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        context.read<AuthBloc>().add(
+                          const AuthEventLogOut(),
+                        );
+                        /*
+                    await AuthService.firebase().logOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                        registerRoute,
+                        (route) => false,
+                    );
+                    */
+                      },
+                      child: const Text("Restart"),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
